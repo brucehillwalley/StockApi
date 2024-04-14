9a47009bdd502c8040f1280af72471d8c3020c9b
BruceHill <brucehillwalley@gmail.com>
2024-04-14 23:55:45 +0300

    product

--- src/models/product.js
+++ src/models/product.js
@@ -1,6 +1,20 @@
 "use strict"
 /* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
+    NODEJS EXPRESS | STOCK MANAGEMENT API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose:{ Schema, model} } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        index: true
+    }
+
+
+
+
+},{ collection:"products", timestamps: true })
+
+module.exports = model("Product", ProductSchema)
