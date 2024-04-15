5502a8000790501bca019717ceec7e49b2bd8683
BruceHill <brucehillwalley@gmail.com>
2024-04-15 23:18:34 +0300

    pre middleware

--- src/models/user.js
+++ src/models/user.js
@@ -6,8 +6,8 @@ const {
   mongoose: { Schema, model },
 } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
-const passwordValidation = require("../helpers/passwordValidation");
-const emailValidation = require("../helpers/emailValidation");
+// const passwordValidation = require("../helpers/passwordValidation");
+// const emailValidation = require("../helpers/emailValidation");
 
 const UserSchema = new Schema(
   {
@@ -23,7 +23,7 @@ const UserSchema = new Schema(
       type: String,
       trim: true,
       required: true,
-      set: (password) => passwordValidation(password), //! password validation and encrypt
+      // set: (password) => passwordValidation(password), //! password validation and encrypt
     },
 
     email: {
@@ -31,7 +31,7 @@ const UserSchema = new Schema(
       trim: true,
       required: true,
       unique: true,
-      set: (email) => emailValidation(email), //! email validation
+      // set: (email) => emailValidation(email), //! email validation
       index: true,
     },
 
@@ -69,7 +69,69 @@ const UserSchema = new Schema(
 );
 
 /* ------------------------------------------------------- */
+// https://mongoosejs.com/docs/middleware.html
+
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+// "save" olayı "updateOne" için çalışmaz.
+UserSchema.pre(['save', 'updateOne'], function (next) {
+    // console.log('pre(save,update) run.')
+    // console.log(this)
+
+    // get data from "this" or "this._update"
+    const data = this?._update || this
+
+    // email@domain.com
+    const isEmailValidated = data.email ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) : true
+
+    if (isEmailValidated) {
+
+        console.log('Email OK')
+
+        if (data?.password) {
+
+            const isPasswordValidated = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(data.password)
+
+            if (isPasswordValidated) {
+
+                console.log('Password OK')
+
+                data.password = passwordEncrypt(data.password)
+
+                if (this?._update) {
+
+                    this._update = data
+                    // this._update.password = data.password 
+
+                } else {
+                    // this = data // izin vermiyor.
+                    this.password = data.password
+                }
+
+                //? ShortHand:
+                // // save:
+                // this.password = data.password = passwordEncrypt(data.password)
+                // // update:
+                // this._update = data
+
+
+            } else {
+                next(new Error('Password is not validated.'))
+            }
+        }
+        next()
+
+    } else {
+        next(new Error('Email is not validated.'))
+    }
+})
+/* ------------------------------------------------------- */
 module.exports = model("User", UserSchema);
+
+
+
+
+
 
 // UserSchema.pre(['save', 'updateOne'], function (next) {
 
