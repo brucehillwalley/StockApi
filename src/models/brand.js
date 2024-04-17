fc2870fc2389a64ec35a52aec4db7378ebd5ba4d
BruceHill <brucehillwalley@gmail.com>
2024-04-17 21:28:27 +0300

    brand

--- src/models/brand.js
+++ src/models/brand.js
@@ -1,6 +1,29 @@
 "use strict"
 /* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
+    NODEJS EXPRESS | STOCK MANAGEMENT API
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+// Brand Model:
+
+const BrandSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    image: {
+        type: String,
+        trim: true,
+    }
+
+}, {
+    collection: 'brands',
+    timestamps: true
+})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Brand', BrandSchema)
